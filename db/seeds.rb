ffdc32e2aba644fb5b85a2c9d80baf4d74147b14
kender <Kender@lia>
2021-04-24 11:02:02 -0600

    Ft add to seed
    - I add some series to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Serie.create(name: "Tate no Yuusha no Nariagari", cover_page: "https://cdn.myanimelist.net/images/anime/1245/111800.jpg")
+Serie.create(name: "Claymore", cover_page: "https://cdn.myanimelist.net/images/anime/3/21834.jpg")
+Serie.create(name: "Elfen Lied", cover_page: "https://cdn.myanimelist.net/images/anime/10/6883.jpg")
+Serie.create(name: "Another", cover_page: "https://cdn.myanimelist.net/images/anime/4/75509.jpg")
+Serie.create(name: "Isekai Maou to Shoukan Shoujo no Dorei Majutsu Ω", cover_page: "https://cdn.myanimelist.net/images/anime/1011/113703.jpg")
+Serie.create(name: "Ijiranaide, Nagatoro-san", cover_page: "https://cdn.myanimelist.net/images/anime/1900/110097.jpg")
+Serie.create(name: "Jujutsu Kaisen (TV)", cover_page: "https://cdn.myanimelist.net/images/anime/1171/109222.jpg")
+Serie.create(name: "One Piece", cover_page: "https://cdn.myanimelist.net/images/anime/6/73245.jpg")
